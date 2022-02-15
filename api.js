246874ebf6df6c1a2e654fbfcbbe76f4ea4d315f
Tejas <tejas.patiltp2802@gmail.com>
2022-02-15 11:56:48 +0530

    APIS

--- api.js
+++ api.js
@@ -2,13 +2,18 @@ let express = require('express');
 let app = express();
 const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
- const mongoUrl = "mongodb://localhost:27017"
-// const mongoUrl = "mongodb+srv://netmeds:netmeds123@cluster0.x6wa1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const mongoUrl = "mongodb://localhost:27017"
+const mongoUrl = "mongodb+srv://netmeds:netmeds123@cluster0.x6wa1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const dotenv = require('dotenv')
 dotenv.config()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 let port = process.env.PORT || 8210;
 var db;
 
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cors())
 //get
 app.get('/',(req,res) => {
     res.send('Welcome to Express')
@@ -23,34 +28,137 @@ app.get('/product',(req,res) => {
     })
 })
 
-//medicines wrt product available
-// app.get('/medicine/:id',(req,res) => {
-//     let medId  = Number(req.params.id)
-//     console.log(">>>>medId",medId)
-//     db.collection('medicine').find({"id":medId}).toArray((err,result) =>{
-//         if(err) throw err;
-//         res.send(result)
-//     })
-// })
 
-app.get('/medicine',(req,res) => {
-    let netmedsId  = Number(req.query.id)
-    let mhaelthcareId = Number(req.query.id)
+//product wrt category
+app.get('/category',(req,res) => {
+    let typeId  = Number(req.query.type_id)
+    let sortId = Number(req.query.sort_id)
     let query = {};
-    if(netmedsId){
-        query = {"mealTypes.mealtype_id":mealId,state_id:stateId}
+    if(typeId){
+        query = {type_id:typeId}
     }
-    else if(stateId){
-        query = {state_id:stateId}
+    else if(sortId){
+        query = {"sortType.sort_id":sortId}
     }
-    else if(mealId){
-        query = {"mealTypes.mealtype_id":mealId}
-    }
-    console.log(">>>>restId",stateId)
-    db.collection('restaurants').find(query).toArray((err,result) =>{
+    console.log(">>>>typeId",typeId)
+    db.collection('category').find(query).toArray((err,result) =>{
         if(err) throw err;
         res.send(result)
     })
+})
+
+//filter
+app.get('/filter/:sortId', (req,res) => {
+    let sort = {cosst:1}
+    let sortId = Number(req.params.sortId)
+    let skip = 0;
+    let limit = 10000000000;
+    let categoryId = Number(req.query.category)
+    let lprice = Number(req.query.lprice);
+    let hprice = Number(req.query.hprice);
+    let query = {}
+    if (req.query.sort){
+        sort:{cost:req.query.sort}
+    }
+    if(req.query.skip && req.query.skip){
+        skip = Number(req.query.skip);
+        limit = Number(req.query.limit)
+    }
+    else if(categoryId&lprice&hprice){
+        query = {
+            "category.category_id":categoryId,
+            "sortType.sort_id":sortId,
+            $and:[{price:{$gt:lprice,$lt:hprice}}]
+        }
+    }
+    else if(categoryId){
+        query = {"category.category_id":categoryId,"sortType.sort_id":sortId}
+    }
+    else if(lprice&hprice){
+        query = {$and:[{price:{$gt:lprice,$lt:hprice}}],"sortType.sort_id":sortId}
+    }
+    db.collection('category').find(query).sort(sort).skip(skip).limit(limit).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//sort
+app.get('/sort',(req,res) => {
+    db.collection('sort').find().toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//product Details
+//product wrt category
+app.get('/details/:id',(req,res) => {
+    let typeId  = Number(req.params.id)
+    console.log(">>>>typeId",typeId)
+    db.collection('category').find({_id:typeId}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//product selected wrt to category
+app.get('/menu/:id',(req,res) => {
+    let typeId  = Number(req.params.id)
+    console.log(">>>>typeId",typeId)
+    db.collection('desc').find({products_id:typeId}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//product selected using user selection
+app.post('/menuItem',(req,res) => {
+    console.log(req.body)
+    db.collection('desc').find({products_id:{$in:req.body}}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+
+//get orders
+app.get('/orders',(req,res) => {
+    let email  = Number(req.query.email)
+    let query = {};
+    if(email){
+        query = {"email":email}
+    }
+    
+    db.collection('orders').find(query).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//place order
+app.post('/placeorder',(req,res) => {
+    db.collection('orders').insert(req.body,(err,result) =>{
+        if(err) throw err;
+        res.send("Order Added")
+    })
+})
+
+//update order 
+app.put('/updateOrder/:id',(req,res) => {
+    let oId = mongo.ObjectId(req.params.id)
+    let status = req.query.status
+    db.collection('orders').updateOne(
+        {_id:oId},
+        {$set:{
+            "status":status,
+            "bank_name":req.body.bank_name,
+            "bank_status":req.body.bank_status
+        }},(err,result) => {
+            if(err) throw err;
+            res.send(`Status Updated To ${status}`) 
+        }
+    )
 })
 
 MongoClient.connect(mongoUrl, (err,connection) => {
--- netmedsapi.md
+++ netmedsapi.md
@@ -2,35 +2,59 @@ First Page{
     > List Of Product Type (eg. Medicine, Healthcare Devices, Nutrition Drinks),
         > http://localhost:6702/product
         > 
-    > List of Medicine or other product wrt product type (eg. If medicine , the list of medicine to be displayed )
-        List of medicine 
-            > http://localhost:6702/medicine/3
-            > 
-        List of healthcare 
-            > 
-            > 
-    > Category data (eg. devices = all the devices should be loaded, if nutrition drink all the drinks should be loaded  on next page)
+    > List of product  wrt category  (eg. If medicine , the list of medicine to be displayed )
+        List of product 
+            > http://localhost:6702/category
+            >
+        List of product wrt category
+            > http://localhost:6702/category?type_id=6
+            >
+    > Top Category
+        > http://localhost:6702/category?sort_id=1
+        >
 }
 
 Second Page{
     > If order medicine (the sort should be open from a to z)
     > If healthcare products (on click on filter data should be displayed wrt the filter chosen)
         Filter
-            > The data to be loaded wrt category
+            > The data to be loaded wrt category + price
+                > http://localhost:6702/filter/1?lprice=1&hprice=200&category_id=1
+                > 
             > The data to be loaded wrt sub category
             > The data to be loaded wrt price 
+                > http://localhost:6702/filter/2?lprice=1&hprice=200
+                    > low to high 
+                        > http://localhost:6702/filter/1?lprice=1&hprice=200&category_id=1&sort=1
+                    > high to low 
+                        > http://localhost:6702/filter/1?lprice=1&hprice=200&category_id=1&sort=-1
+            > pagination
+                > http://localhost:6702/filter/1?category_id=1&skip=4&limit=2
+                > 
 }
 
 Third Page{
-    Product Details (description > uses) 2 apis separate 
-            > medicine 
-            > healthcare devices
+    Product Details
+        > http://localhost:6702/details/3
+        > 
+
+    All the products (menu)   
+        > http://localhost:6702/menu/101   
 }
 
 Fourth Page{
+    product on basis of user selection
+        > localhost:6702/menuItem
+        > 
+        > body [3]
     Api to place order
+        > localhost:6702/placeorder
 }
 
 Fifth Page{
     Place all orders
+        > localhost:6702/placeorder
 }
+
+Update Orders   
+    > localhost:6702/updateOrder/620b4170a6d255a37b47f6fa?status=success
